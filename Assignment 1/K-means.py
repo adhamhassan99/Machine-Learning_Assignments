19810a4daa56c72f3bcad2a61316578533085aae
Adham <Adhamhassan100@yahoo.com>
2022-02-27 06:25:14 +0200

    first commit

---
+++ Assignment 1/K-means.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd, seaborn as sns, matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_moons
+
+my_data = pd.read_csv("C:\\Users\Adham\\Documents\\Machine Learning\\Machine-Learning_Assignments\\Assignment 1\\Customer data.csv")
+# print(my_data.head())
+
+def GUC_Distance(Cluster_Centroids, Data_points, Distance_Type):
+    pass
+
+def GUC_Kmean ( Data_points, Number_of_Clusters,  Distance_Type ):
+       
+    
+    return [ Final_Cluster_Distance , Cluster_Metric ]
